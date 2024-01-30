99dfea6d7b3ee4e2b6d9576e1a2c21319c056459
GemmaLatte <ljh990424@gmail.com>
2024-01-30 11:01:44 +0900

    01-30

---
+++ frontend copy/react-dobongi/src/components/Header.js
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header className="bg-gray-50 border-solid border-2">
+      <nav id="header-nav">
+        <ul>
+          <li>
+            <a href="#home">홈</a>
+          </li>
+          <li>
+            <a href="#news">도봉이 근황 보기</a>
+          </li>
+          <li>
+            <a href="#contact">도봉이 만든 사람 소개</a>
+          </li>
+          <li>
+            <a href="#about">도봉이는...</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
